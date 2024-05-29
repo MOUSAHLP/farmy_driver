30a0cd7bf617e8b9999dab679ee180461f430bf8
mousa hlp <mousahlp@gmail.com>
2024-05-29 15:41:47 +0300

    fix pdf

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -2,211 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Order;
-use App\Models\User;
-use App\Services\OrderService;
-use App\Traits\CoreRequests;
-use Illuminate\Http\Request;
-use App\Traits\NotificationHelper;
-use App\Enums\NotificationsTypes;
-use App\Models\Notification;
-use App\Enums\OrderStatus;
+use App\Http\Requests\OrderDetailRequest;
+use App\Services\OrderDetailService;
 
 class OrderDetailController extends Controller
 {
-    use CoreRequests;
-    public function __construct(private OrderService $orderService)
+    public function __construct(private OrderDetailService $orderDetailService)
     {
     }
 
-    public function acceptOrderByDriver($order_id)
+    public function getDriverOrders()
     {
+        $data = $this->orderDetailService->getDriverOrders();
 
-        $order = Order::find($order_id);
-        if (!$order) {
-            return $this->errorResponse(
-                "orders.NotFound",
-                404
-            );
-        }
-
-        $res = $this->orderService->acceptOrderByDriver($order);
-
-        if ($res) {
-            return $this->successResponse(
-                null,
-                'orders.Accepted'
-            );
-        } else {
-            return $this->errorResponse(
-                "orders.Already_Accepted",
-                404
-            );
-        }
-    }
-    public function checkCodeExists(Request $request,$id)
-    {
-
-        $code = $request->input('code');
-
-        $order = Order::where('code', $code)->first();
-
-
-        if ($order) {
-
-
-              return response()->json( [$order,"code is true"]);
-        } else {
-
-         return response()->json([$order,"code is false"]);
-        }
-
-    }
-    public function rejectOrderByDriver($order_id)
-    {
-        $order = Order::find($order_id);
-        if (!$order) {
-            return $this->errorResponse(
-                "orders.NotFound",
-                404
-            );
-        }
-
-        $res = $this->orderService->rejectOrderByDriver($order);
-
-        if ($res) {
-            return $this->successResponse(
-                null,
-                'orders.Rejected'
-            );
-        } else {
-            return $this->errorResponse(
-                "orders.Already_Rejected",
-                404
-            );
-        }
-    }
-
-      public function deliverOrderByDriver(Request $request,$order_id)
-    {
-
-        $code = $request->input('code');
-
-        $order = Order::find($order_id);
-        if (!$order) {
-            return $this->errorResponse(
-                "orders.NotFound",
-                404
-            );
-        }
-      if (empty($code))  {
-
-               $order = Order::find($order_id);
-                $client = User::find($order->user_id);
-           $data = [
-               "title" => __("messages.orders.OrderOnWay.title"),
-               "body" => __($order->code),
-           ];
-           NotificationHelper::sendPushNotification([$client->fcm_token], $data, NotificationsTypes::PushNotifications);
-           Notification::create([
-               'type'            =>  NotificationsTypes::PushNotifications,
-               'notifiable_type' => 'App\Models\User',
-               'notifiable_id'   => $order->user_id,
-               'data'            => $data,
-           ]);
-           $order->update([
-            "status" => OrderStatus::OnDelivery
-        ]);
-    return $this->successResponse(
-                $data,
-                'orders.OnDelivery'
-            );
-    }
-        $order = Order::where('code', $code)->first();
-
-        if ($order) {
-
-
-        $res = $this->orderService->deliverOrderByDriver($order);
-
-        if ($res) {
-             $order = Order::find($order_id);
-                $client = User::find($order->user_id);
-        //   $data = [
-        //       "title" => __("messages.orders.OrderOnWay.title"),
-        //       "body" => __($order->code),
-        //   ];
-        //   NotificationHelper::sendPushNotification([$client->fcm_token], $data, NotificationsTypes::PushNotifications);
-        //   Notification::create([
-        //       'type'            =>  NotificationsTypes::PushNotifications,
-        //       'notifiable_type' => 'App\Models\User',
-        //       'notifiable_id'   => $order->user_id,
-        //       'data'            => $data,
-        //   ]);
-            $data["order_total"] = $order->total;
-            return $this->successResponse(
-                $data,
-                'orders.Delivered'
-            );
-        } else {
-            return $this->errorResponse(
-                "orders.OtherDriver",
-                400
-            );
-        }
-    }
-
-   else{
-        return $this->errorResponse(
-            "CodeError",
-            400
+        return $this->successResponse(
+            $data,
+            'dataFetchedSuccessfully'
         );
     }
-    }
-
-    public function makeOrderPaid($order_id)
+    public function getDriverOrderDetail($order_id)
     {
-        $order = Order::find($order_id);
-        if (!$order)  return $this->errorResponse("orders.NotFound", 404);
+        $data = $this->orderDetailService->getDriverOrderDetail($order_id);
 
-        $res = $this->orderService->makeOrderPaid($order);
-
-        if ($res) {
-            return $this->successResponse(
-                null,
-                'orders.Paid'
-            );
-        } else {
-            return $this->errorResponse(
-                "orders.Already_Paid",
-                400
-            );
-        }
+        return $this->successResponse(
+            $data,
+            'dataFetchedSuccessfully'
+        );
     }
-
-    public function acceptAssignedOrderByDriver($order_id)
+    public function updateDriverOrderDetail($order_id, OrderDetailRequest $request)
     {
+        $this->orderDetailService->updateDriverOrderDetail($order_id, $request);
 
-        $order = Order::find($order_id);
-        if (!$order) {
-            return $this->errorResponse(
-                "orders.NotFound",
-                404
-            );
-        }
-
-        $res = $this->orderService->acceptAssignedOrderByDriver($order);
-
-        if ($res) {
-            return $this->successResponse(
-                null,
-                'orders.Accepted'
-            );
-        } else {
-            return $this->errorResponse(
-                "orders.Already_Accepted",
-                404
-            );
-        }
+        return $this->successResponse(
+            null,
+            'dataUpdatedSuccessfully'
+        );
     }
 }
